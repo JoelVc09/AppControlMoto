87874a076b59336093320a550e1f0b0b1d4b947c
JoelVc09 <Joelvch09@gmail.com>
2024-10-02 01:36:49 -0500

    Se manejó la opcion de cerrar sesion, se realiso la eliminacion de las preferencia de email y password, seguidamente redirigiendo a Loguin

--- app/src/main/java/com/example/appcontrolmoto/Home.kt
+++ app/src/main/java/com/example/appcontrolmoto/Home.kt
@@ -1,7 +1,9 @@
 package com.example.appcontrolmoto
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -49,6 +51,24 @@ class Home : Fragment() {
         btnScanner.setOnClickListener {
             val intent = Intent(requireActivity(), LectorQr::class.java)
             startActivity(intent)
+        }
+
+        val btnLogout: Button = view.findViewById(R.id.btnLogout)
+        // Acción para el botón de cerrar sesión
+        btnLogout.setOnClickListener {
+            // Eliminar las preferencias de "email" y "password"
+            val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+            val editor = sharedPreferences.edit()
+            editor.remove("email")    // Eliminar el correo
+            editor.remove("password") // Eliminar la contraseña
+            editor.apply() // Aplica los cambios
+
+            // Redirigir a la actividad de Loguin
+            val intent = Intent(requireActivity(), Loguin::class.java)
+            startActivity(intent)
+
+            // Finalizar la actividad actual si no quieres volver a ella
+            requireActivity().finish()
         }
 
         return view
